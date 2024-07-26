4cc667b1acd725ca4db98449e23e371785d22bcf
Graham Batzler <graham.batzler@hearst.com>
2024-07-26 15:03:51 -0700

    basic redesign to simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,129 +1,83 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { slugify } from "./utils";
 
-// Main App component
-function App() {
+const App = () => {
   const [inputs, setInputs] = useState<string[]>([""]);
-  const [isCopied, setIsCopied] = useState(false);
+  const [output, setOutput] = useState("");
 
-  useEffect(() => {
-    // Read initial state from the URL hash
-    const handleHashChange = () => {
-      const hash = decodeURI(window.location.hash.substring(1));
-      const segments = hash.split("/").filter((segment) => segment !== "");
-      if (segments && segments.some((s) => ![undefined, ""].includes(s))) {
-        setInputs(segments);
-      }
-    };
-
-    // handle base case of data already in the URL
-    handleHashChange();
-
-    window.addEventListener("hashchange", handleHashChange);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange);
-    };
-  }, []);
-
-  useEffect(() => {
-    // Update URL hash when inputs state changes
-    const newHash = `/${inputs.filter((slug) => slug !== "").join("/")}`;
-    window.location.hash = newHash;
-  }, [inputs]);
-
-  const handleInputChange = (index: number, value: string) => {
-    const newInputs = [...inputs];
-    newInputs[index] = value;
-    setInputs(newInputs);
-  };
-
-  const handleRemoveInput = (indexToRemove: number) => {
-    const newInputs = [...inputs];
-    newInputs.splice(indexToRemove, 1);
-    if (newInputs.length > 0) {
+  const handleInputChange =
+    (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newInputs = [...inputs];
+      newInputs[index] = e.target.value;
       setInputs(newInputs);
-    } else {
-      setInputs([""]);
-    }
+      updateOutput(newInputs);
+    };
+
+  const addInputField = () => {
+    setInputs([...inputs, ""]);
   };
 
-  const copyToClipboard = (text: string) => {
-    console.log(`copied "${text}"`);
-    navigator.clipboard.writeText(text).then(
-      () => {
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 2000);
-      },
-      (err) => {
-        console.error(err);
-      },
-    );
+  const removeInputField = (index: number) => () => {
+    const newInputs = inputs.filter((_, i) => i !== index);
+    setInputs(newInputs);
+    updateOutput(newInputs);
   };
 
-  const concatUrl = inputs
-    .map(slugify)
-    .filter((slug) => slug !== "")
-    .join("/");
+  const updateOutput = (inputs: string[]) => {
+    const slugifiedStrings = inputs.map((input) => slugify(input));
+    setOutput(slugifiedStrings.join("/"));
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(output);
+    alert("Copied to clipboard!");
+  };
 
   return (
-    <div style={{ width: "100%" }}>
-      <h1>VTEX URL Slug Builder</h1>
-      <table style={{ width: "100%" }}>
-        <thead>
-          <tr>
-            <th style={{ width: "45%" }}>Category</th>
-            <th style={{ width: "50%" }}>Slug</th>
-            <th style={{ width: "5%" }}></th>
-          </tr>
-        </thead>
-        <tbody>
+    <div className="container">
+      <h1>URL Slug Builder</h1>
+      <div className="module-wrapper">
+        <div className="input-group-container">
           {inputs.map((input, index) => (
-            <tr key={index}>
-              <td>
-                <input
-                  type="text"
-                  value={inputs[index] || ""}
-                  onChange={(e) => handleInputChange(index, e.target.value)}
-                />
-              </td>
-              <td>{slugify(inputs[index] || "")}</td>
-              {inputs[index] !== "" && (
-                <td
-                  style={{ cursor: "pointer" }}
-                  onClick={() => handleRemoveInput(index)}
+            <div key={index} className="input-group">
+              <input
+                type="text"
+                value={input}
+                onChange={handleInputChange(index)}
+                placeholder="Category, collection, or product title"
+                className="input-field"
+              />
+              {Boolean(input) && (
+                <button
+                  onClick={removeInputField(index)}
+                  disabled={inputs.length === 1}
+                  className="square-button"
+                  aria-label="Remove"
                 >
-                  ❌
-                </td>
+                  &#10006;
+                </button>
               )}
-            </tr>
+            </div>
           ))}
-        </tbody>
-      </table>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <button
-          onClick={() => setInputs([...inputs, ""])}
-          disabled={inputs[inputs.length - 1] === ""}
-        >
-          + Add Segment
+        </div>
+        <button onClick={addInputField} >
+          + Another one
         </button>
       </div>
-      <hr />
-      <div style={{ marginTop: "10px" }}>
-        <h2>Concatenated URL</h2>
-        <pre>{concatUrl ? `/${concatUrl}` : "--"}</pre>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <button
-            onClick={() => copyToClipboard(`/${concatUrl}`)}
-            disabled={concatUrl == ""}
-          >
-            {isCopied ? "Copied!" : "Copy to Clipboard"}
-          </button>
+      <div className="module-wrapper">
+        <div className={`output ${Boolean(output) ? "" : "placeholder"}`}>
+          {output}
         </div>
+        <button
+          onClick={copyToClipboard}
+          disabled={!output}
+          
+        >
+          Copy to Clipboard
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
