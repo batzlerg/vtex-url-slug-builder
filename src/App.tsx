1c816b643df98a19c8a3038076c63a4fbbd0c818
Graham Batzler <graham.batzler@hearst.com>
2024-07-26 15:35:39 -0700

    fix styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { slugify } from "./utils";
+import VtexLogo from "./VtexLogo";
 
 const App = () => {
   const [inputs, setInputs] = useState<string[]>([""]);
@@ -25,7 +26,7 @@ const App = () => {
 
   const updateOutput = (inputs: string[]) => {
     const slugifiedStrings = inputs.map((input) => slugify(input));
-    setOutput(slugifiedStrings.join("/"));
+    setOutput(slugifiedStrings.length ? `/${slugifiedStrings.join("/")}` : "");
   };
 
   const copyToClipboard = () => {
@@ -35,22 +36,25 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1>URL Slug Builder</h1>
-      <div className="module-wrapper">
-        <div className="input-group-container">
+      <div className="header">
+        <VtexLogo className="logo" />
+        <h1>URL Slug Builder</h1>
+      </div>
+      <div className="module">
+        <div className="input-container">
           {inputs.map((input, index) => (
-            <div key={index} className="input-group">
+            <div key={index} className="input-row">
               <input
                 type="text"
                 value={input}
                 onChange={handleInputChange(index)}
-                placeholder="Category, collection, or product title"
+                placeholder="Add a category, collection, or product title"
                 className="input-field"
               />
               {Boolean(input) && (
                 <button
                   onClick={removeInputField(index)}
-                  disabled={inputs.length === 1}
+                  // disabled={inputs.length === 1}
                   className="square-button"
                   aria-label="Remove"
                 >
@@ -60,18 +64,18 @@ const App = () => {
             </div>
           ))}
         </div>
-        <button onClick={addInputField} >
+        <button onClick={addInputField} className="full-width" disabled={!inputs[inputs.length - 1]}>
           + Another one
         </button>
       </div>
-      <div className="module-wrapper">
+      <div className="module">
         <div className={`output ${Boolean(output) ? "" : "placeholder"}`}>
           {output}
         </div>
         <button
           onClick={copyToClipboard}
           disabled={!output}
-          
+          className="full-width"
         >
           Copy to Clipboard
         </button>
